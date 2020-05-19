d2d49c071393101a58c83b3564566b41455c19fa
yaowuya <zrfyaowuya@126.com>
2020-05-19 22:18:57 +0800

    修改数据库连接

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 module.exports = app => {
     const mongoose=require("mongoose");
 
-    mongoose.connect("mongodb://127.0.0.1:27017/shopping",{
+    mongoose.connect("mongodb://127.0.0.1:27017/question",{
         useNewUrlParser:true
     });
 
