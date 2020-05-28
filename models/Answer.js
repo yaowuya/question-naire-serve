8375541053c7d8784e6aae6440788d44ace3b56f
yaowuya <zrfyaowuya@126.com>
2020-05-28 22:34:59 +0800

    问卷页面

--- models/Answer.js
+++ models/Answer.js
@@ -5,6 +5,7 @@ const schema = new mongoose.Schema({
     person: {type: mongoose.SchemaTypes.ObjectId, ref: 'Person'},
     question: {type: mongoose.SchemaTypes.ObjectId, ref: 'Question'},
     option: [{type: mongoose.SchemaTypes.ObjectId, ref: 'Option'}],
+    content: {type: String},
     createTime: {type: Date, default: Date.now()},
     updateTime: {type: Date, default: Date.now()}
 }, {
